63f5fc0fda04fcb0842903169042bafee3c4fb6d
Sumaiya Asif <sumaiya.aa@gmail.com>
2017-03-13 12:37:14 -0500

    Initial Commit

---
+++ ServerProgram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ServerProgram
+//
+//  Created by Sumaiya Asif on 3/13/17.
+//  Copyright © 2017 Sumaiya Asif. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
